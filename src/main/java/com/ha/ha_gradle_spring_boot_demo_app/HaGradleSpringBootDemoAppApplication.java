78ebbeb461becc1a4b25464fce6b020eca4ae3a9
hasiabewardana <hasi.prasanga@gmail.com>
2024-10-30 07:55:15 +0530

    Initial commit

---
+++ src/main/java/com/ha/ha_gradle_spring_boot_demo_app/HaGradleSpringBootDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ha.ha_gradle_spring_boot_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaGradleSpringBootDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaGradleSpringBootDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ha/ha_gradle_spring_boot_demo_app/HaGradleSpringBootDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ha.ha_gradle_spring_boot_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaGradleSpringBootDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
